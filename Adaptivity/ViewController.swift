32876015c16c6d50af090e1ba225fe66c4a24e3d
Stephen Anthony <steve@darjeelingapps.com>
2015-12-15 15:06:00 +0000

    Initial Commit

---
+++ Adaptivity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Adaptivity
+//
+//  Created by Stephen Anthony on 15/12/2015.
+//  Copyright © 2015 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
