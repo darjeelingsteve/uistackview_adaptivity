5bf2512e15467cc3513e5fd188945578c9aa31e4
Stephen Anthony <steve@darjeelingapps.com>
2016-01-25 22:01:40 +0000

    Use @noescape for completion handler

--- Adaptivity/Spotlight/SpotlightController.swift
+++ Adaptivity/Spotlight/SpotlightController.swift
@@ -43,7 +43,7 @@ class SpotlightController: NSObject {
         }
     }
     
-    func handleUserActivity(userActivity: NSUserActivity, completionHandler: (County?) -> Void) {
+    func handleUserActivity(userActivity: NSUserActivity, @noescape completionHandler: (County?) -> Void) {
         var selectedCounty: County? = nil
         if userActivity.activityType == CSSearchableItemActionType {
             selectedCounty = County.allCounties.filter({$0.name == userActivity.title}).first
