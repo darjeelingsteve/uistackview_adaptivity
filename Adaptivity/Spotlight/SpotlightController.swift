5f4b3f43270ea7f7602523c6d85b7ee59ec4ac8d
Stephen Anthony <steve@darjeelingapps.com>
2016-11-07 17:04:40 +0000

    Use larger flag thumbnail in Spotlight

--- Adaptivity/Spotlight/SpotlightController.swift
+++ Adaptivity/Spotlight/SpotlightController.swift
@@ -29,7 +29,7 @@ class SpotlightController: NSObject, CountyUserActivityHandling {
             attributeSet.supportsNavigation = 1
             if let countyFlag = county.flagImage {
                 // Scale image as recommended by https://developer.apple.com/library/ios/documentation/General/Conceptual/AppSearch/SearchUserExperience.html#//apple_ref/doc/uid/TP40016308-CH11-SW1
-                attributeSet.thumbnailData = UIImagePNGRepresentation(countyFlag.scaledImageWithWidth(270))
+                attributeSet.thumbnailData = UIImagePNGRepresentation(countyFlag.scaledImageWithWidth(300))
             }
             let searchableItem = CSSearchableItem(uniqueIdentifier: county.name, domainIdentifier: nil, attributeSet: attributeSet)
             return searchableItem
