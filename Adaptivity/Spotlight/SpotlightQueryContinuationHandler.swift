9f7a26a519a1bbc48f0a2d347ecfb0a090b09260
Stephen Anthony <steve@darjeelingapps.com>
2016-11-08 09:55:18 +0000

    Add SpotlightQueryContinuationHandler

---
+++ Adaptivity/Spotlight/SpotlightQueryContinuationHandler.swift
@@ -0,0 +1,23 @@
+//
+//  SpotlightQueryContinuationHandler.swift
+//  Adaptivity
+//
+//  Created by Stephen Anthony on 07/11/2016.
+//  Copyright © 2016 Darjeeling Apps. All rights reserved.
+//
+
+import Foundation
+import CoreSpotlight
+
+class SpotlightQueryContinuationHandler: UserActivityHandling {
+    var handledActivityType: String {
+        return CSQueryContinuationActionType
+    }
+    
+    func resultFromUserActivity(_ userActivity: NSUserActivity) -> UserActivityHandlingResult? {
+        guard let searchText = userActivity.userInfo?[CSSearchQueryString] as? String else {
+            return nil
+        }
+        return .searchText(searchText: searchText)
+    }
+}
