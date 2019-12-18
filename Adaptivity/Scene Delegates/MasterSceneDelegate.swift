f6924b53c37f97b1b574928d45c34e9e1549e5f6
Stephen Anthony <steve@darjeelingapps.com>
2019-12-18 15:52:45 +0000

    Add MasterSceneDelegate

---
+++ Adaptivity/Scene Delegates/MasterSceneDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  MasterSceneDelegate.swift
+//  Counties
+//
+//  Created by Stephen Anthony on 18/12/2019.
+//  Copyright © 2019 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+/// The delegate of the application's master scene.
+class MasterSceneDelegate: UIResponder {
+    var window: UIWindow?
+    private let history = CountyHistory()
+    private let spotlightController = SpotlightController()
+    private var applicationShortcutHandler: ApplicationShortcutHandler?
+    private let userActivityHandlers: [UserActivityHandling]
+    
+    override init() {
+        userActivityHandlers = [spotlightController, HandoffController(), SpotlightQueryContinuationHandler()]
+        super.init()
+        history.delegate = self
+    }
+}
+
+// MARK: UIWindowSceneDelegate
+extension MasterSceneDelegate: UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let navigationController = window?.rootViewController as? UINavigationController, let masterViewController = navigationController.topViewController as? MasterViewController {
+            masterViewController.history = history
+            applicationShortcutHandler = ApplicationShortcutHandler(masterViewController: masterViewController)
+        }
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        var handled = false
+        // Loop over our user activity handlers to handle the activity
+        for userActivityHandler in userActivityHandlers {
+            userActivityHandler.handleUserActivity(userActivity, completionHandler: { (result) -> Void in
+                handled = true
+                dismissExistingCountyViewIfRequired({ (masterViewController) -> (Void) in
+                    switch result {
+                    case .county(let county):
+                        masterViewController.showCounty(county, animated: false)
+                    case .searchText(let searchText):
+                        masterViewController.beginSearch(withText: searchText)
+                    }
+                })
+            })
+            if handled {
+                // The user activity was handled so we don't need to query any more activity handlers
+                break
+            }
+        }
+    }
+    
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        dismissExistingCountyViewIfRequired { [unowned self] (masterViewController) -> (Void) in
+            self.applicationShortcutHandler?.handle(shortcutItem, completionHandler: completionHandler)
+        }
+    }
+    
+    private func dismissExistingCountyViewIfRequired(_ completion: (MasterViewController) -> (Void)) {
+        let navigationController = window?.rootViewController as! UINavigationController
+        // Dismiss any existing county that is being shown
+        navigationController.dismiss(animated: false, completion: nil)
+        let viewController = navigationController.topViewController as! MasterViewController
+        completion(viewController)
+    }
+}
+
+// MARK: CountyHistoryDelegate
+extension MasterSceneDelegate: CountyHistoryDelegate {
+    func countyHistoryDidUpdate(_ countyHistory: CountyHistory) {
+        UIApplication.shared.shortcutItems = countyHistory.recentlyViewedCounties.map({ (county) -> UIApplicationShortcutItem in
+            return UIApplicationShortcutItem(type: CountyItemShortcutType, localizedTitle: county.name)
+        })
+    }
+}
