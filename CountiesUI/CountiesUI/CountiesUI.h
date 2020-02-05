6cfffb99b77ac12bb11a422dfa63ba728b339c9d
Stephen Anthony <steve@darjeelingapps.com>
2020-02-05 16:46:26 +0000

    Add CountyUI framework

---
+++ CountiesUI/CountiesUI/CountiesUI.h
@@ -0,0 +1,19 @@
+//
+//  CountiesUI.h
+//  CountiesUI
+//
+//  Created by Stephen Anthony on 05/02/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountiesUI.
+FOUNDATION_EXPORT double CountiesUIVersionNumber;
+
+//! Project version string for CountiesUI.
+FOUNDATION_EXPORT const unsigned char CountiesUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountiesUI/PublicHeader.h>
+
+
