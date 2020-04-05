75c51a81a596d97fe49576e98f92299f13d371cc
Stephen Anthony <steve@darjeelingapps.com>
2020-04-05 20:39:48 +0100

    Perform Spotlight indexing on Utility queue

--- CountiesModel/Sources/Spotlight Support/SpotlightController.swift
+++ CountiesModel/Sources/Spotlight Support/SpotlightController.swift
@@ -20,40 +20,27 @@ public class SpotlightController {
             return
         }
         
-        let searchableItems = counties.map { (county) -> CSSearchableItem in
-            let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeData as String)
-            attributeSet.title = county.name
-            attributeSet.contentDescription = county.populationDescription
-            attributeSet.latitude = county.latitude as NSNumber?
-            attributeSet.longitude = county.longitude as NSNumber?
-            attributeSet.supportsNavigation = 1
-            if let countyFlag = county.flagImage {
-                // Scale image as recommended by https://developer.apple.com/library/ios/documentation/General/Conceptual/AppSearch/SearchUserExperience.html#//apple_ref/doc/uid/TP40016308-CH11-SW1
-                attributeSet.thumbnailData = countyFlag.scaledImageWithWidth(300).pngData()
+        DispatchQueue.global(qos: .utility).async {
+            let searchableItems = counties.map { (county) -> CSSearchableItem in
+                let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeData as String)
+                attributeSet.title = county.name
+                attributeSet.contentDescription = county.populationDescription
+                attributeSet.latitude = county.latitude as NSNumber?
+                attributeSet.longitude = county.longitude as NSNumber?
+                attributeSet.supportsNavigation = 1
+                if let countyFlag = county.flagImage {
+                    attributeSet.thumbnailData = countyFlag.pngData()
+                }
+                let searchableItem = CSSearchableItem(uniqueIdentifier: county.name, domainIdentifier: nil, attributeSet: attributeSet)
+                return searchableItem
             }
-            let searchableItem = CSSearchableItem(uniqueIdentifier: county.name, domainIdentifier: nil, attributeSet: attributeSet)
-            return searchableItem
-        }
-        
-        let searchableIndex = CSSearchableIndex.default()
-        searchableIndex.indexSearchableItems(searchableItems) { (error) -> Void in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-            }
-        }
-    }
-}
 
-private extension UIImage {
-    func scaledImageWithWidth(_ width: CGFloat) -> UIImage {
-        let imageScale = width / size.width
-        let newSize = size.applying(CGAffineTransform(scaleX: imageScale, y: imageScale))
-        
-        UIGraphicsBeginImageContextWithOptions(size, true, 0.0)
-        draw(in: CGRect(origin: CGPoint.zero, size: newSize))
-        
-        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return scaledImage!
+            let searchableIndex = CSSearchableIndex.default()
+            searchableIndex.indexSearchableItems(searchableItems) { (error) -> Void in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        }
     }
 }
