59f76d0a5846803a606c31d3938ecf7f13c7658c
Stephen Anthony <steve@darjeelingapps.com>
2020-04-22 20:04:33 +0100

    Adjust table style cell heights for different content size categories

--- CountiesUI/Sources/County Menu/CountiesCollectionViewController.swift
+++ CountiesUI/Sources/County Menu/CountiesCollectionViewController.swift
@@ -223,7 +223,7 @@ private extension CountyCell.DisplayStyle {
     func itemSizeInCollectionView(_ collectionView: UICollectionView) -> CGSize {
         switch self {
         case .table:
-            return CGSize(width: collectionView.bounds.width, height: 44)
+            return CGSize(width: collectionView.bounds.width, height: tableCellHeight(for: collectionView.traitCollection.preferredContentSizeCategory))
         case .grid:
             let availableWidth = collectionView.bounds.width - collectionView.layoutMargins.left - collectionView.layoutMargins.right
             let interitemSpacing = (collectionView.collectionViewLayout as! UICollectionViewFlowLayout).minimumInteritemSpacing
@@ -238,6 +238,31 @@ private extension CountyCell.DisplayStyle {
             
             let itemWidth = floor((availableWidth - totalSpacingBetweenAdjacentItems) / CGFloat(numberOfItemsPerRow))
             return CGSize(width: itemWidth, height: floor(itemWidth / 1.3))
+        }
+    }
+    
+    private func tableCellHeight(for contentSizeCategory: UIContentSizeCategory) -> CGFloat {
+        switch contentSizeCategory {
+        case .extraSmall, .small, .medium, .large, .unspecified:
+            return 44
+        case .extraLarge:
+            return 48
+        case .extraExtraLarge:
+            return 52
+        case .extraExtraExtraLarge:
+            return 58
+        case .accessibilityMedium:
+            return 69
+        case .accessibilityLarge:
+            return 81
+        case .accessibilityExtraLarge:
+            return 97
+        case .accessibilityExtraExtraLarge:
+            return 114
+        case .accessibilityExtraExtraExtraLarge:
+            return 127
+        default:
+            return tableCellHeight(for: .unspecified)
         }
     }
     
