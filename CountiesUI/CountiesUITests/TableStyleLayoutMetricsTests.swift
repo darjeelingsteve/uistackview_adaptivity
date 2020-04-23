b3059257d1b1f731119ceaf11ab921505517b9f3
Stephen Anthony <steve@darjeelingapps.com>
2020-04-23 20:02:06 +0100

    Add TableStyleLayoutMetrics

---
+++ CountiesUI/CountiesUITests/TableStyleLayoutMetricsTests.swift
@@ -0,0 +1,139 @@
+//
+//  TableStyleLayoutMetricsTests.swift
+//  CountiesUITests
+//
+//  Created by Stephen Anthony on 23/04/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+import XCTest
+@testable import CountiesUI
+
+final class TableStyleLayoutMetricsTests: XCTestCase {
+    private var metrics: TableStyleLayoutMetrics!
+    
+    override func tearDown() {
+        metrics = nil
+        super.tearDown()
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheExtraSmallContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .extraSmall)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 12, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 32)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 6)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheSmallContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .small)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 12, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 32)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 6)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheMediumContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .medium)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 12, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 32)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 6)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .large)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 13, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 38)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 7)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .extraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 15, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 44)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 8)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheExtraExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .extraExtraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 17, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 50)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 8)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheExtraExtraExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .extraExtraExtraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 19, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 56)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 9)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheAccessibilityMediumContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .accessibilityMedium)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 23, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 72)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 11)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheAccessibilityLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .accessibilityLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 27, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 84)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 11)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheAccessibilityExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .accessibilityExtraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 33, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 105)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 14)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheAccessibilityExtraExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .accessibilityExtraExtraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 38, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 124)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 17)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheExpectedMetricsForTheAccessibilityExtraExtraExtraLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .accessibilityExtraExtraExtraLarge)
+        XCTAssertEqual(metrics.sectionHeaderFont, .systemFont(ofSize: 44, weight: .regular))
+        XCTAssertEqual(metrics.sectionHeaderHeight, 142)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), 18)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), 38)
+    }
+    
+    func testItReturnsTheSameMetricsForTheUnsepcifiedContentSizeCategoryAsForTheLargeContentSizeCategory() {
+        givenLayoutMetrics(forContentSizeCategory: .unspecified)
+        let largeMetrics = TableStyleLayoutMetrics(contentSizeCategory: .large)
+        XCTAssertEqual(metrics.sectionHeaderFont, largeMetrics.sectionHeaderFont)
+        XCTAssertEqual(metrics.sectionHeaderHeight, largeMetrics.sectionHeaderHeight)
+        XCTAssertEqual(metrics.sectionHeaderLabelBottomPadding, largeMetrics.sectionHeaderLabelBottomPadding)
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: false), largeMetrics.sectionBottomPadding(forSectionThatIsTheLastSection: false))
+        XCTAssertEqual(metrics.sectionBottomPadding(forSectionThatIsTheLastSection: true), largeMetrics.sectionBottomPadding(forSectionThatIsTheLastSection: true))
+    }
+    
+    private func givenLayoutMetrics(forContentSizeCategory contentSizeCategory: UIContentSizeCategory) {
+        metrics = TableStyleLayoutMetrics(contentSizeCategory: contentSizeCategory)
+    }
+}
---
+++ CountiesUI/Sources/County Menu/TableStyleLayoutMetrics.swift
@@ -0,0 +1,116 @@
+//
+//  TableStyleLayoutMetrics.swift
+//  CountiesUI iOS
+//
+//  Created by Stephen Anthony on 23/04/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+/// A struct representing the various layout metrics used when laying out a
+/// collection view in a grouped table style for a given content size category.
+struct TableStyleLayoutMetrics {
+    private static let defaultContentSizeCategory: UIContentSizeCategory = .large
+    
+    private let contentSizeCategory: UIContentSizeCategory
+    
+    init(contentSizeCategory: UIContentSizeCategory) {
+        self.contentSizeCategory = contentSizeCategory
+    }
+    
+    /// The font to use for section headers.
+    var sectionHeaderFont: UIFont {
+        switch contentSizeCategory {
+        case .extraSmall, .small, .medium:
+            return .systemFont(ofSize: 12, weight: .regular)
+        case .large:
+            return .systemFont(ofSize: 13, weight: .regular)
+        case .extraLarge:
+            return .systemFont(ofSize: 15, weight: .regular)
+        case .extraExtraLarge:
+            return .systemFont(ofSize: 17, weight: .regular)
+        case .extraExtraExtraLarge:
+            return .systemFont(ofSize: 19, weight: .regular)
+        case .accessibilityMedium:
+            return .systemFont(ofSize: 23, weight: .regular)
+        case .accessibilityLarge:
+            return .systemFont(ofSize: 27, weight: .regular)
+        case .accessibilityExtraLarge:
+            return .systemFont(ofSize: 33, weight: .regular)
+        case .accessibilityExtraExtraLarge:
+            return .systemFont(ofSize: 38, weight: .regular)
+        case .accessibilityExtraExtraExtraLarge:
+            return .systemFont(ofSize: 44, weight: .regular)
+        case .unspecified:
+            fallthrough
+        default:
+            return TableStyleLayoutMetrics(contentSizeCategory: TableStyleLayoutMetrics.defaultContentSizeCategory).sectionHeaderFont
+        }
+    }
+    
+    /// The height to use for section headers.
+    var sectionHeaderHeight: CGFloat {
+        switch contentSizeCategory {
+        case .extraSmall, .small, .medium:
+            return 32
+        case .large:
+            return 38
+        case .extraLarge:
+            return 44
+        case .extraExtraLarge:
+            return 50
+        case .extraExtraExtraLarge:
+            return 56
+        case .accessibilityMedium:
+            return 72
+        case .accessibilityLarge:
+            return 84
+        case .accessibilityExtraLarge:
+            return 105
+        case .accessibilityExtraExtraLarge:
+            return 124
+        case .accessibilityExtraExtraExtraLarge:
+            return 142
+        case .unspecified:
+            fallthrough
+        default:
+            return TableStyleLayoutMetrics(contentSizeCategory: TableStyleLayoutMetrics.defaultContentSizeCategory).sectionHeaderHeight
+        }
+    }
+    
+    /// The padding to apply between the bottom of the section header title
+    /// label and the bottom of the section header view.
+    var sectionHeaderLabelBottomPadding: CGFloat {
+        switch contentSizeCategory {
+        case .extraSmall, .small, .medium:
+            return 6
+        case .large:
+            return 7
+        case .extraLarge, .extraExtraLarge:
+            return 8
+        case .extraExtraExtraLarge:
+            return 9
+        case .accessibilityMedium, .accessibilityLarge:
+            return 11
+        case .accessibilityExtraLarge:
+            return 14
+        case .accessibilityExtraExtraLarge:
+            return 17
+        case .accessibilityExtraExtraExtraLarge:
+            return 18
+        case .unspecified:
+            fallthrough
+        default:
+            return TableStyleLayoutMetrics(contentSizeCategory: TableStyleLayoutMetrics.defaultContentSizeCategory).sectionHeaderLabelBottomPadding
+        }
+    }
+    
+    /// The padding to apply to the bottom of a section.
+    /// - Parameter sectionIsLastSection: A `Boolean` value indicating whether
+    /// the padding is for the last section in the collection view or not.
+    /// - Returns: The height to use for the section's bottom padding.
+    func sectionBottomPadding(forSectionThatIsTheLastSection sectionIsLastSection: Bool) -> CGFloat {
+        return sectionIsLastSection ? 38 : 18
+    }
+}
