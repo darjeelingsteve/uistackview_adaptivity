36d0bf67f1cc623c4156bad026b7260d891d06f1
Stephen Anthony <steve@darjeelingapps.com>
2020-10-03 14:24:49 +0100

    Add SwiftUI watch app

---
+++ Counties (watchOS) WatchKit Extension/AdaptivityApp.swift
@@ -0,0 +1,20 @@
+//
+//  AdaptivityApp.swift
+//  Counties (watchOS) WatchKit Extension
+//
+//  Created by Stephen Anthony on 03/10/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AdaptivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
